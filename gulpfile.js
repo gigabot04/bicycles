f02cc19915e322ea8439351a0c2592a580c1dc36
Костя <GigaBOT4@yandex.ru>
2021-02-19 13:03:21 +0300

    Графика

--- gulpfile.js
+++ gulpfile.js
@@ -42,7 +42,7 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/js/**/*.js", gulp.series("uglify", "refresh"))
+  gulp.watch("source/js/**/*.js", gulp.series("refresh"))
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
