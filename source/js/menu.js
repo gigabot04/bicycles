420556afd90635e1b4609c1f2fd09062bd9abe19
gigabot04 <64404363+gigabot04@users.noreply.github.com>
2021-02-20 18:32:08 +0300

    Delete source directory

--- source/js/menu.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-{
-  const headerMenuBtn = document.querySelector(`.header__btn-menu`);
-  const headerMenu = document.querySelector(`.header__nav`);
-  headerMenu.classList.remove(`header__nav--nojs`);
-  headerMenuBtn.addEventListener(`click`, () => {
-    headerMenu.classList.toggle(`header__nav--active`);
-  });
-}
