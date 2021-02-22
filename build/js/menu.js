07e3aa55cb134779729c8faf715a8955cf2847ca
Костя <GigaBOT4@yandex.ru>
2021-02-23 01:22:31 +0300

    Исправил замечания

--- build/js/menu.js
+++ build/js/menu.js
@@ -3,7 +3,9 @@
   const headerMenuBtn = document.querySelector(`.header__btn-menu`);
   const headerMenu = document.querySelector(`.header__nav`);
   headerMenu.classList.remove(`header__nav--nojs`);
+  headerMenuBtn.classList.remove(`header__btn-menu--nojs`);
   headerMenuBtn.addEventListener(`click`, () => {
     headerMenu.classList.toggle(`header__nav--active`);
+    headerMenuBtn.classList.toggle(`header__btn-menu--active`);
   });
 }
