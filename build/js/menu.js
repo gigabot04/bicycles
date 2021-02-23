62563757b93a13ebf8c3e4a13323b5ee4596221b
gigabot04 <64404363+gigabot04@users.noreply.github.com>
2021-02-23 15:05:08 +0300

    Add files via upload

--- build/js/menu.js
+++ build/js/menu.js
@@ -1,9 +1,13 @@
 'use strict';
 {
   const headerMenuBtn = document.querySelector(`.header__btn-menu`);
-  const headerMenu = document.querySelector(`.header__nav`);
-  headerMenu.classList.remove(`header__nav--nojs`);
+  const headerMenu = document.querySelector(`.header__nav-wrapper`);
+  const body = document.querySelector(`body`);
+  headerMenu.classList.remove(`header__nav-wrapper--nojs`);
+  headerMenuBtn.classList.remove(`header__btn-menu--nojs`);
   headerMenuBtn.addEventListener(`click`, () => {
-    headerMenu.classList.toggle(`header__nav--active`);
+    headerMenu.classList.toggle(`header__nav-wrapper--active`);
+    body.classList.toggle(`body--js`);
+    headerMenuBtn.classList.toggle(`header__btn-menu--active`);
   });
 }
