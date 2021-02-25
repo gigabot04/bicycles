70810e81919bf279ce7b63b5f08945fbefb631cb
Костя <GigaBOT4@yandex.ru>
2021-02-25 16:29:56 +0300

    Исправил баг

--- source/js/menu.js
+++ source/js/menu.js
@@ -5,9 +5,20 @@
   const body = document.querySelector(`body`);
   headerMenu.classList.remove(`header__nav-wrapper--nojs`);
   headerMenuBtn.classList.remove(`header__btn-menu--nojs`);
-  headerMenuBtn.addEventListener(`click`, () => {
+
+  const openCloseMenu = () => {
     headerMenu.classList.toggle(`header__nav-wrapper--active`);
     body.classList.toggle(`body--js`);
     headerMenuBtn.classList.toggle(`header__btn-menu--active`);
+  };
+
+  headerMenuBtn.addEventListener(`click`, () => {
+    openCloseMenu();
+  });
+  headerMenu.addEventListener(`click`, (evt) => {
+    let trg = evt.target;
+    if (trg.tagName === `A`) {
+      openCloseMenu();
+    }
   });
 }
